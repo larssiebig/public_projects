7ecb563011001f508f1bf242e088be8c383a9692
larssiebig <lars.siebig.ls@gmail.com>
2022-11-19 01:25:28 +0100

    first push

---
+++ Python/Todolist/Todolist.py
@@ -0,0 +1,79 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import messagebox
+
+def newTask(e):
+    task = my_entry.get()
+    if task != "":
+        lb.insert(END, task)
+        my_entry.delete(0, "end")
+    else:
+        messagebox.showwarning("warning", "Please enter some task.")
+
+
+def deleteTask():
+    lb.delete(ANCHOR)
+    
+ws = Tk()
+ws.geometry('500x450+500+200')
+ws.title('PythonGuides')
+ws.config(bg='#223441')
+ws.resizable(width=False, height=False)
+ws.bind('<Return>', newTask)
+
+
+frame = Frame(ws)
+frame.pack(pady=10)
+
+lb = Listbox(
+    frame,
+    width=25,
+    height=8,
+    font=('Times', 18),
+    bd=0,
+    fg='#ffffff',
+    highlightthickness=0,
+    selectbackground='#68c0d4',
+    activestyle="none",
+    
+)
+lb.pack(side=LEFT, fill=BOTH)
+
+task_list = [] 
+    
+
+for item in task_list:
+    lb.insert(END, item)
+
+sb = Scrollbar(frame)
+sb.pack(side=RIGHT, fill=BOTH)
+
+lb.config(yscrollcommand=sb.set)
+sb.config(command=lb.yview)
+
+my_entry = Entry(
+    ws,
+    font=('times', 24)
+    )
+
+my_entry.pack(pady=20)
+
+button_frame = Frame(ws)
+button_frame.pack(pady=20)
+
+Label(ws ,text="Press enter to add a Task", 
+          font=('times 14'), bg='#223441').pack(pady=20)
+
+delTask_btn = Button(
+    button_frame,
+    text='Delete Task',
+    font=('times 14'),
+    bg='#ff8b61',
+    padx=20,
+    pady=10,
+    command=deleteTask
+)
+delTask_btn.pack(fill=BOTH, expand=True, side=BOTTOM)
+
+
+ws.mainloop()
